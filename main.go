839c9c1e83ef1a388abdcdd1dc616fb3002855ba
Daan <daan.vdj@gmail.com>
2020-09-25 16:24:50 +0200

    refactor: remove unfinished parse

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
-	"io"
-	"log"
-	"os"
-
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
@@ -83,37 +77,6 @@ func PostMovie(c *gin.Context) {
 	}
 
 	// curl -i -X POST -H "Content-Type: application/json" -d "{ \"imdbid\": \"tt0816692\", \"name\": \"InterStellar\", \"year\": \"2014\", \"imdbscore\": \"8,6\"  }" http://localhost:8080/api/v1/movies
-}
-
-// ParsWatchList Parses a watchlist and adds it to the db
-func ParseWatchList(c *gin.Context) {
-	db := InitDb()
-	defer db.Close()
-
-	// Open the file
-	csvfile, err := os.Open("watchlist.csv")
-	if err != nil {
-		log.Fatalln("Couldn't open the csv file", err)
-	}
-
-	// Parse the file
-	r := csv.NewReader(csvfile)
-	//r := csv.NewReader(bufio.NewReader(csvfile))
-
-	// Iterate through the records
-	for {
-		// Read each record from csv
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("ImdbID: %s Name: %s Year: %s ImdbScore: %s\n", record[1], record[5], record[10], record[8])
-
-	}
 }
 
 // GetMovies Gets all movies in db
